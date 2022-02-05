6af4768130998cd371aa5231e304491fe8f49b17
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2022-02-05 16:07:27 +0700

    refactor: refactor sidebar on member area

---
+++ components/organism/Sidebar/MenuItem.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import cx from 'classnames';
+
+interface MenuItemProps {
+  title: string;
+  icon:
+    | 'ic-menu-overview'
+    | 'ic-menu-transactions'
+    | 'ic-menu-messages'
+    | 'ic-menu-card'
+    | 'ic-menu-rewards'
+    | 'ic-menu-settings'
+    | 'ic-menu-logout';
+  active?: boolean;
+}
+
+export default function MenuItem(props: Partial<MenuItemProps>) {
+  const { title, icon, active } = props;
+  const classItem = cx({ item: true, active, 'mb-30': true });
+  return (
+    <div className={classItem}>
+      <div className='me-3'>
+        <Image src={`/icon/${icon}.svg`} width={25} height={25} />
+      </div>
+      <p className='item-title m-0'>
+        <a href='' className='text-lg text-decoration-none'>
+          {title}
+        </a>
+      </p>
+    </div>
+  );
+}
