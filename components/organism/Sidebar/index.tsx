098363c05c6255eacba9cb6c5643f10f39f6a31a
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2022-02-06 17:47:26 +0700

    refactor: add active menu props into sidebar

--- components/organism/Sidebar/index.tsx
+++ components/organism/Sidebar/index.tsx
@@ -2,7 +2,20 @@ import Footer from './Footer';
 import MenuItem from './MenuItem';
 import Profile from './Profile';
 
-export default function Sidebar() {
+interface SidebarProps {
+  activeMenu:
+    | 'overview'
+    | 'transactions'
+    | 'messages'
+    | 'card'
+    | 'rewards'
+    | 'settings'
+    | 'logout';
+}
+
+export default function Sidebar(props: SidebarProps) {
+  const { activeMenu } = props;
+
   return (
     <section className='sidebar'>
       <div className='content pt-50 pb-30 ps-30'>
@@ -12,20 +25,37 @@ export default function Sidebar() {
             title='Overview'
             icon='ic-menu-overview'
             href='/member'
-            active
+            active={activeMenu === 'overview'}
           />
           <MenuItem
             title='Transactions'
             icon='ic-menu-transactions'
             href='/member/transactions'
+            active={activeMenu === 'transactions'}
           />
-          <MenuItem title='Messages' icon='ic-menu-messages' href='/member' />
-          <MenuItem title='Card' icon='ic-menu-card' href='/member' />
-          <MenuItem title='Rewards' icon='ic-menu-rewards' href='/member' />
+          <MenuItem
+            title='Messages'
+            icon='ic-menu-messages'
+            href='/member'
+            active={activeMenu === 'messages'}
+          />
+          <MenuItem
+            title='Card'
+            icon='ic-menu-card'
+            href='/member'
+            active={activeMenu === 'card'}
+          />
+          <MenuItem
+            title='Rewards'
+            icon='ic-menu-rewards'
+            href='/member'
+            active={activeMenu === 'rewards'}
+          />
           <MenuItem
             title='Settings'
             icon='ic-menu-settings'
             href='/member/edit-profile'
+            active={activeMenu === 'settings'}
           />
           <MenuItem
             title='Logout'
